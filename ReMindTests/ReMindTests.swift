e5b9436b76ea7e650d4125a3a6641a62f47aca74
Gonzalo Trujillo Vallejo <gonzalo.trujillo@babelgroup.com>
2024-11-16 12:57:21 +0100

    Initial Commit

---
+++ ReMindTests/ReMindTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReMindTests.swift
+//  ReMindTests
+//
+//  Created by Gonzalo Trujillo Vallejo on 16/11/24.
+//
+
+import Testing
+@testable import ReMind
+
+struct ReMindTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
